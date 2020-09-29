64ea5f5afe4e8170af8ffedb46999ae833901c8d
Abraao Lincoln <abraao.lincol@gmail.com>
2020-09-29 18:02:29 -0300

    add menu dropdown

--- views/static/taskManager/js/clientServer.js
+++ views/static/taskManager/js/clientServer.js
@@ -18,18 +18,83 @@ function createTask(task){
         return divTask;
     };
 
+    let createMenu = () => {
+        let divDropdown = document.createElement("DIV");
+        divDropdown.className = "dropdown";
+        //Create menu icon
+        let menuIcon =  document.createElement("I");
+        menuIcon.className = "fas fa-ellipsis-v";
+        divDropdown.appendChild(menuIcon);
+        let divMenu1 = document.createElement("DIV");
+        divMenu1.className = "dropdown-content";
+        //Create buttons move up and down
+        let btnMoveUp = document.createElement("BUTTON");
+        btnMoveUp.type = "button";
+        btnMoveUp.className = "btnDropdown1 litleadJutment";
+        btnMoveUp.innerText = "Mover para cima";
+        let btnMoveDown = document.createElement("BUTTON");
+        btnMoveDown.type = "button";
+        btnMoveDown.className = "btnDropdown1 litleadJutment";
+        btnMoveDown.innerText = "Mover para baixo";
+        divMenu1.appendChild(btnMoveUp);
+        divMenu1.appendChild(btnMoveDown);
+        //Create submenu
+        let divDropdown2 = document.createElement("DIV");
+        divDropdown2.className = "btnDropdown1 dropdown2";
+        //Create button dropdown2
+        let btnMoveTo = document.createElement("BUTTON");
+        btnMoveTo.type = "button";
+        btnMoveTo.className = "btnDropdown1-2";
+        btnMoveTo.innerText = "Mover para:";
+        divDropdown2.appendChild(btnMoveTo);
+        //Create content dropdown2
+        let divMenu2 = document.createElement("DIV");
+        divMenu2.className = "dropdown2-content";
+        //Create buttons menu2
+        let btnMoveToNormal = document.createElement("INPUT");
+        btnMoveToNormal.type = "button";
+        btnMoveToNormal.className = "btnDropdown2";
+        btnMoveToNormal.value = "normal";
+        btnMoveToNormal.placeholder = "Normal";
+        btnMoveToNormal.addEventListener('click', moveTaskTo);
+        let btnMoveToInProgress = document.createElement("INPUT");
+        btnMoveToInProgress.type = "button";
+        btnMoveToInProgress.className = "btnDropdown2";
+        btnMoveToInProgress.value = "andamento";
+        btnMoveToInProgress.placeholder = "Andamento";
+        btnMoveToInProgress.addEventListener('click', moveTaskTo);
+        let btnMoveToFinished = document.createElement("INPUT");
+        btnMoveToFinished.type = "button";
+        btnMoveToFinished.className = "btnDropdown2";
+        btnMoveToFinished.value = "completada";
+        btnMoveToFinished.placeholder = "Completada";
+        btnMoveToFinished.addEventListener('click', moveTaskTo);
+        divMenu2.appendChild(btnMoveToNormal);
+        divMenu2.appendChild(btnMoveToInProgress);
+        divMenu2.appendChild(btnMoveToFinished);
+        divDropdown2.appendChild(divMenu2);
+        //Mounting the dropdown
+        divMenu1.appendChild(divDropdown2);
+        //Final step
+        divDropdown.appendChild(divMenu1);
+
+        return divDropdown;
+    }
+
     let createDivTaskHead = () => {
-        let icon = document.createElement("I");
+        // let icon = document.createElement("I");
+        let menu = createMenu();
         let title = document.createElement("H3");
         let deleteButton = document.createElement("SPAN");
         let divTaskHead = document.createElement("DIV");
         divTaskHead.className = "taskHead";
-        icon.className = "fas fa-ellipsis-v";
+        // icon.className = "fas fa-ellipsis-v";
         title.innerHTML = task.name;
         deleteButton.className = "spanDelete";
         deleteButton.innerHTML = "X";
         deleteButton.addEventListener("click", hideTask);
-        divTaskHead.appendChild(icon);
+        // divTaskHead.appendChild(icon);
+        divTaskHead.appendChild(menu);
         divTaskHead.appendChild(title);
         divTaskHead.appendChild(deleteButton);
 
@@ -70,16 +135,19 @@ function createTask(task){
     return newTask;
 }
 
-function addTaskToList(state, newTask){
+function addTaskToList(state, newTask, taskId){
     switch(state){
         case "normal":
             document.getElementById("normal").appendChild(newTask);
+            document.getElementById(taskId).style.backgroundColor = "#f6f8ff";
             break;
         case "andamento":
             document.getElementById("andamento").appendChild(newTask);
+            document.getElementById(taskId).style.backgroundColor = "#218380";
             break;
         case "completada":
             document.getElementById("completada").appendChild(newTask);
+            document.getElementById(taskId).style.backgroundColor = "#266dd3";
             break;
     }
 }
@@ -90,11 +158,11 @@ function addNewTask(event){
         name: document.getElementById("newTaskName").value,
         due: document.getElementById("newTaskDate").value,
         responsible: document.getElementById("newTaskResponsible").value,
-        state: "normal"
+        state: document.getElementById("stateNewTask").value
     }
     tasks.push(newTask);
     let newTaskElement = createTask(newTask);
-    addTaskToList(newTask.state, newTaskElement);
+    addTaskToList(newTask.state, newTaskElement, newTask.name);
 
     //Cleanig
     document.getElementById("newTaskName").value = "";
@@ -106,10 +174,30 @@ function hideTask(event){
     document.getElementById(event.target.parentNode.parentNode.id).style.display = "none";
 }
 
+function moveTaskTo(event){
+    let newState = event.target.value;
+    let taskId = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+
+    switch(newState){
+        case "normal":
+            document.getElementById("normal").appendChild(document.getElementById(taskId));
+            document.getElementById(taskId).style.backgroundColor = "#f6f8ff";
+            break;
+        case "andamento":
+            document.getElementById("andamento").appendChild(document.getElementById(taskId));
+            document.getElementById(taskId).style.backgroundColor = "#218380";
+            break;
+        case "completada":
+            document.getElementById("completada").appendChild(document.getElementById(taskId));
+            document.getElementById(taskId).style.backgroundColor = "#266dd3";
+            break;
+    }
+}
+
 function start(){
     for(t of tasks){
         let task = createTask(t);
-        addTaskToList(t.state, task);
+        addTaskToList(t.state, task, t.name);
     }
 }
 
